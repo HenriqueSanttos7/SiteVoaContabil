3d1a6d82b30b9cc17748e7147d82ddb1497f34b3
Vitoria <vitoriapassos42@gmail.com>
2026-02-19 08:45:02 -0400

    ajuste site

--- public/assets/js/loadFooter.js
+++ public/assets/js/loadFooter.js
@@ -3,14 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   if (!footerContainer) return;
 
   fetch("/components/footer.html")
-    .then( (response) => {
-      console.log("Resposta do fetch:", response);
+    .then((response) => {
       if (!response.ok) throw new Error("Erro ao carregar o footer");
       return response.text();
     })
     .then((html) => {
       footerContainer.innerHTML = html;
+
+      // 🔹 Inicializações do footer
+      initFooterYear();
+
       if (typeof AOS !== "undefined") AOS.refresh();
     })
     .catch((err) => console.error("Falha ao carregar footer:", err));
+
+  function initFooterYear() {
+    const yearSpan = document.getElementById("currentYear");
+    if (yearSpan) {
+      yearSpan.textContent = new Date().getFullYear();
+    }
+  }
 });
